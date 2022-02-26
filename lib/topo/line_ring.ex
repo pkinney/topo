624cac360e20a4cc7b418250826ea997fec45271
Powell Kinney <628952+pkinney@users.noreply.github.com>
2022-02-26 10:59:45 -0600

    Fixes issue where linestrings that had a segment outside of a ring were incorrectly reported as contained (#19)

--- lib/topo/line_ring.ex
+++ lib/topo/line_ring.ex
@@ -12,7 +12,8 @@ defmodule Topo.LineRing do
   @spec contains?(list, list) :: boolean
   def contains?(ring, line) do
     (PointRing.any_point?(ring, line, :interior) || any_midpoint_interior?(ring, line)) &&
-      !PointRing.any_point?(ring, line, :disjoint)
+      !PointRing.any_point?(ring, line, :disjoint) &&
+      !any_segment?(ring, line, :interior)
   end
 
   defp any_midpoint_interior?(_, [_]), do: false
@@ -20,6 +21,19 @@ defmodule Topo.LineRing do
   defp any_midpoint_interior?(a, [b1, b2 | rest]) do
     PointRing.relate(a, Util.midpoint(b1, b2)) == :interior ||
       any_midpoint_interior?(a, [b2 | rest])
+  end
+
+  defp any_segment?(_, [_], _), do: false
+
+  defp any_segment?(a, [b1, b2 | rest], relation) do
+    any_ring_segment?(a, b1, b2, relation) || any_segment?(a, [b2 | rest], relation)
+  end
+
+  defp any_ring_segment?([_], _, _, _), do: false
+
+  defp any_ring_segment?([a1, a2 | rest], b1, b2, relation) do
+    SegSeg.intersection(a1, a2, b1, b2) |> elem(1) == relation ||
+      any_ring_segment?([a2 | rest], b1, b2, relation)
   end
 
   @spec none_contain_line?(list, list) :: boolean
